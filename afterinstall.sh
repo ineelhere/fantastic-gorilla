8d6db976aa48e6bf7f79cd69dc1b4bbfd8c69e43
ineelhere <you@example.com>
2022-07-29 23:52:04 +0530

    [ADD] fix awsaspec yml, add a nice lanng page

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd /home/ubuntu
-npm -f install
+sudo apt-get -y update
+sudo apt-get -y install python3-pip
+python3 --version
--- applicationstart.sh
+++ applicationstart.sh
@@ -1,6 +1,7 @@
-
-
 #!/bin/bash
 
 cd /home/ubuntu
-pm2 -f start server.js
+pip3 install django
+python3 manage.py migrate
+sudo apt -y install screen
+screen -d -m python3 manage.py runserver 0.0.0.0:8000
